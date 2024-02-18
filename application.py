7483f5ac054c7b3960664c1cba7a65d5e4629880
Derek Taylor <taylor.3537@osu.edu>
2024-02-18 09:17:45 -0500

    pulls a number from openai

--- application.py
+++ application.py
@@ -18,7 +18,27 @@ def pullImage(str):
     newjson = response.model_dump_json()
     url = parseResponse(newjson)
     return url
-    
+
+def pullInt():
+    response = client.chat.completions.create(
+        model = "gpt-3.5-turbo",
+        messages= [{"role":"user", "content":"give me a random integer between 0 and 10000"}]
+
+    )
+    dump =  response.model_dump_json()
+    num = parseInt(dump)
+    return num
+
+
+def parseInt(dump):
+    inte = ''
+    place  = dump.find('content')
+    dump = dump[place+9:len(dump)]
+    pl2 = dump.find(',')
+    inte = dump[0:pl2]
+    num = eval(inte)
+    return num
+
 def parseResponse(response):
     url = ''
     ln = len(response)
@@ -26,5 +46,5 @@ def parseResponse(response):
     url = response[place+6:ln-4]
     return url
 
+pullInt()
 
-pullImage("a cat with a hat")
